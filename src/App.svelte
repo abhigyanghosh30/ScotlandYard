9b84748eb711184b76693514a4b1b9f36fc2a316
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2021-06-09 21:51:46 +0530

    added sample map

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,15 @@
 <script>
+	import LondonMap from './map_final.svelte';
 	let playertype = 'D'; // D for detective, T for thief
-
 	let data = {'nodevalue':null,'ticket':null};
 	var socket = io();
+
+	function handleClick(){
+		event.preventDefault();
+		data['nodevalue']=event.target.id;
+		console.log(data);
+	}
+	
 	function handleSubmit(){
 		event.preventDefault();
 		if (data['nodevalue']>0) {
@@ -23,28 +30,14 @@
 	}
 </script>
 <main>
+	<LondonMap/>
 	<form on:submit={handleSubmit}>
-		<input type="number" max="200" min="1" value={data["nodevalue"]} on:change={handleChange} name="nodevalue" required/>
+		<!-- <input type="number" max="200" min="1" value={data["nodevalue"]} on:change={handleChange} name="nodevalue" required/>
 		<select on:change={handleChange} name="ticket" required>
 			<option value="T">Taxi</option>
 			<option value="B">Bus</option>
 			<option value="U">Underground</option>
-		</select>
+		</select> -->
 		<button type="submit">Submit</button>
 	</form>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
